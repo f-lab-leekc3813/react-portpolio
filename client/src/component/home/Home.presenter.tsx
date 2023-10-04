9891166ae9d694c625e8886c498308132deacc5e
1715leekc <1715leekc@naver.com>
2023-10-04 17:20:05 +0900

    Feat: main-section2

--- client/src/component/home/Home.presenter.tsx
+++ client/src/component/home/Home.presenter.tsx
@@ -12,9 +12,35 @@ export default function HomePageUI() {
                     <h1 className={styles.headingPrimary}>
                         <span className={styles.headingPrimaryMain}>OutDoors</span>
                         <span className={styles.headingPrimarySub}>is where life happens</span>
-                    </h1>
+                    </h1>    
+                </div>
+            </div>
+            <div className={styles.sectionAbout}>
+                <div className={`${styles.uCenterText} ${styles.uMarginBottomBig}`}>
+                    <h2 className={styles.headingSecondary}>
+                        Exciting tours for adventurous people
+                    </h2>
+                </div>
+                <div className={styles.row}>
+                    <div className={styles.col_1_of_2}>
+                        <h3 className={`${styles.headingTertiary} ${styles.uMarginBottomSmall}`}>You're going to fall in love with nature</h3>
+                        <p className={styles.paragraph}>
+                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam ex nostrum voluptatem nesciunt cumque? Rerum ullam recusandae officia, laudantium esse enim voluptatibus magni quam numquam voluptate molestiae explicabo quidem doloremque.
+                        </p>
+                        <h3 className={`${styles.headingTertiary} ${styles.uMarginBottomSmall}`}>Live adventures like you never have before</h3>
+                        <p className={styles.paragraph}>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                        </p>
 
-                    
+                        <button  className={styles.btnText}>Learn more &rarr;</button>
+                    </div>
+                    <div className={styles.col_1_of_2}>
+                        <div className={styles.composition}>
+                            <img src="img/home/nat-1-large.jpg" alt="Photo1" className={`${styles.compositionPhoto} ${styles.compositionPhotoP1}`} />
+                            <img src="img/home/nat-2-large.jpg" alt="Photo2" className={`${styles.compositionPhoto} ${styles.compositionPhotoP2}`} />
+                            <img src="img/home/nat-3-large.jpg" alt="Photo3" className={`${styles.compositionPhoto} ${styles.compositionPhotoP3}`} />
+                        </div>    
+                    </div>
                 </div>
             </div>
         </div>
